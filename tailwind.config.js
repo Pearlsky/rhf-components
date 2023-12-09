5b5c546acf4ee385d2ae97ea5313b04b18b07f59
Pearl Akpan <pearlakpan115@gmail.com>
2023-12-09 23:18:46 +0100

    build(tailwind): add and configure tailwind for styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,js,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
